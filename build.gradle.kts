cce4f4f794ec5dfc418373193648611073820f52
cc-jhr <34243889+cc-jhr@users.noreply.github.com>
2024-01-16 21:09:32 +0100

    Set JDK to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ val sources: List<File> =
 
 coverallsJacoco {
     reportSourceSets = sources
-    reportPath = "${layout.buildDirectory}/reports/jacoco/test/jacocoFullReport.xml"
+    reportPath = "${layout.buildDirectory.get().asFile.absolutePath}/reports/jacoco/test/jacocoFullReport.xml"
 }
 
 tasks.jacocoTestReport {
@@ -56,7 +56,7 @@ tasks.jacocoTestReport {
     reports {
         html.required.set(false)
         xml.required.set(true)
-        xml.outputLocation.set(file("${layout.buildDirectory}/reports/jacoco/test/jacocoFullReport.xml"))
+        xml.outputLocation.set(file("${layout.buildDirectory.get().asFile.absolutePath}/reports/jacoco/test/jacocoFullReport.xml"))
     }
     dependsOn(allprojects.map { it.tasks.named<Test>("test") })
 }
